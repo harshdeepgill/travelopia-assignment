585027f9a7a71703132f3a2c97ca9a611bdc494c
unknown <er.harshdeepgill@gmail.com>
2024-02-02 20:48:53 +0530

    animations added

---
+++ src/pages/landing/components/Button.jsx
@@ -0,0 +1,11 @@
+
+
+function Button() {
+    return ( 
+        <div>
+            <a className="button" href="#">Click Me!</a>
+        </div>
+     );
+}
+
+export default Button;
--- src/pages/landing/sections/HeroSection.jsx
+++ src/pages/landing/sections/HeroSection.jsx
@@ -1,3 +1,4 @@
+import Button from "../components/Button";
 import HeroTitle from "../components/HeroTitle";
 
 
@@ -7,12 +8,12 @@ function HeroSection() {
             <div className="hero-main__image">
             <img 
     srcSet="/resources/small-screen-bg.jpg 480w,
-    /resources/medium-screen-bg.jpg 768w, 
-    /resources/full-screen-bg.jpg 900w"
-    sizes="(max-width: 480px) 480px,
-            (max-width: 768px) 768px,
-            (max-width: 900px) 900px,
-            1200px"
+            /resources/medium-screen-bg.jpg 768w, 
+            /resources/full-screen-bg.jpg 900w"
+    sizes="1200px,
+            (min-width: 480px) 480px,
+            (min-width: 768px) 768px,
+            (min-width: 900px) 900px"
     src="" 
     alt="camping near fire mountains travel" 
 />
@@ -20,6 +21,7 @@ function HeroSection() {
             <div className="hero-main__overlay"></div>
             <div className="hero-main__header">
                 <HeroTitle/>
+                <Button/>
             </div>
         </div>
      );
