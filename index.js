03c7c3743ba6b1b580ca7f75eaccc5c3c563b63e
unknown <er.harshdeepgill@gmail.com>
2024-02-15 08:48:05 +0530

    comments added

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 let btn = document.querySelectorAll(".button");
 let popup = document.getElementById("popup");
 
+// Apllying event listner to the buttons responsible for popup to pop-out or hide.
+
 for(let i = 0;i<btn.length;i++){
     btn[i].addEventListener("click", () => {
         popup.classList.toggle("popup__hidden");
